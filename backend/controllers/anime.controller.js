32864a12d980452674c686da119ccc86912a3419
Andrii Kapran (Work Acc) <a.kapran@do-it.co>
2023-04-20 19:02:17 +0200

    added in anime controller error handlers

--- backend/controllers/anime.controller.js
+++ backend/controllers/anime.controller.js
@@ -4,27 +4,34 @@ const Quote = require("./../models/quote.model");
 
 const imgHelpers = require("./../helpers/image");
 
-const getAnime = (req, res, next) => {
-  Anime.find().then((dataTable) => {
-    res.status(200).json({ status: "Success", animeList: dataTable });
-  });
+const getAnime = async (req, res, next) => {
+  try {
+    const animeList = await Anime.find();
+    res.status(200).json({ status: "success", animeList });
+  } catch (err) {
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
 };
 
-const getAnimeNames = (req, res, next) => {
-  Anime.find()
-    .select("name")
-    .then((list) => {
-      const animeList = list.map(el => {
-        return { id: el._id, text: el.name }
-      })
-      res.status(200).json({ status: "Success", animeList });
+const getAnimeNames = async (req, res, next) => {
+  try {
+    const list = await Anime.find().select("name");
+    const animeList = list.map((el) => {
+      return { id: el._id, text: el.name };
     });
+    res.status(200).json({ status: "Success", animeList });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ status: "Error", message: "Unable to get anime list" });
+  }
 };
 
 const addNewAnime = async (req, res, next) => {
   const reqBody = req.body;
 
   const existingAnime = await Anime.findOne({ name: reqBody.name });
+
   if (existingAnime) {
     return res.status(400).json({ message: "Anime name already exists" });
   }
@@ -35,16 +42,17 @@ const addNewAnime = async (req, res, next) => {
     stars: reqBody.stars,
     status: reqBody.status,
     time: reqBody.time,
-    genres: reqBody && reqBody.genres ? reqBody.genres : "",
+    genres: reqBody?.genres || "",
     heroes: [],
     quotes: [],
   });
 
   try {
     const createdAnime = await newAnime.save();
-    res.status(201).json({ message: "Anime was added successfully", anime: createdAnime });
+    res
+      .status(201)
+      .json({ message: "Anime was added successfully", anime: createdAnime });
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: "Failed to add anime" });
   }
 };
@@ -55,16 +63,16 @@ const deleteAnime = async (req, res, next) => {
   try {
     const anime = await Anime.findById(id);
 
-    if (anime && anime.heroes && anime.heroes.length > 0) {
-      const heroIds = anime.heroes.map((hero) => hero.id);
-
-      anime.heroes.forEach((el) => {
-        imgHelpers.removeImage(el.imageUrl);
+    if (anime?.heroes?.length > 0) {
+      const heroIds = anime.heroes.map((hero) => {
+        imgHelpers.removeImage(hero.imageUrl); //remove hero img from storage
+        return hero.id;
       });
+
       await Hero.deleteMany({ _id: { $in: heroIds } });
     }
 
-    if (anime && anime.quotes && anime.quotes.length > 0) {
+    if (anime?.quotes?.length > 0) {
       await Quote.deleteMany({ _id: { $in: anime.quotes } });
     }
 
@@ -72,39 +80,53 @@ const deleteAnime = async (req, res, next) => {
 
     res.status(200).json({ message: "Anime was removed successfully" });
   } catch (err) {
-    console.error(err);
     res.status(500).json({ message: "Failed to remove anime" });
   }
 };
 
-const editAnime = (req, res, next) => {
+const editAnime = async (req, res, next) => {
   const animeId = req.params.id;
   const reqBody = req.body;
 
-  Anime.findById(animeId)
-    .then((anime) => {
-      anime.name = reqBody.name;
-      anime.nameUA = reqBody.nameUA;
-      anime.stars = reqBody.stars;
-      anime.status = reqBody.status;
-      anime.time = reqBody.time;
-      anime.genres = reqBody && reqBody.genres ? reqBody.genres : "";
-      anime.heroes = anime && anime.heroes ? anime.heroes : [];
-      anime.quotes = anime && anime.quotes ? anime.quotes : [];
+  try {
+    const updatedAnime = await Anime.findById(animeId);
+    if (!updatedAnime) {
+      return res.status(404).json({ message: "Anime not found" });
+    }
 
-      return anime.save();
-    })
-    .then((updatedAnime) => {
-      res.json({ message: "Anime updated successfully", anime: updatedAnime });
+    updatedAnime.name = reqBody.name.trim();
+    updatedAnime.nameUA = reqBody.nameUA.trim();
+    updatedAnime.stars = reqBody.stars;
+    updatedAnime.status = reqBody.status;
+    updatedAnime.time = reqBody.time;
+    updatedAnime.genres = reqBody?.genres || "";
+
+    await updatedAnime.save();
+
+    res.json({
+      message: "Anime updated successfully",
+      anime: updatedAnime,
     });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to edit anime" });
+  }
 };
 
-const getAnimeById = (req, res, next) => {
+const getAnimeById = async (req, res, next) => {
   const id = req.params.id;
 
-  Anime.find({ _id: id }).then((anime) => {
+  try {
+    const anime = await Anime.find({ _id: id });
+    if (!anime) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "Anime not found" });
+    }
+
     res.status(200).json({ status: "Success", anime: anime[0] });
-  });
+  } catch (err) {
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
 };
 
 module.exports = {
