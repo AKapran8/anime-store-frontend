52a8f3f67883b09606508c380e63211a82aa72f2
Andrii Kapran (Work Acc) <a.kapran@do-it.co>
2023-04-28 15:04:21 +0200

    changed module and routing positions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../routings/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
 
-import { AppComponent } from '../app-component/app.component';
-import { DeleteDialogComponent } from '../components/delete-dialog/delete-dialog.component';
-import { HeaderComponent } from '../components/header/header.component';
+import { AppComponent } from './app-component/app.component';
+import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.component';
+import { HeaderComponent } from './components/header/header.component';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { HomeModule } from '../components/home/home.module';
-import { AnimeModule } from '../components/anime/anime.module';
-import { HeroesModule } from '../components/heroes/heroes.module';
-import { QuotesModule } from '../components/quotes/quotes.module';
+import { HomeModule } from './components/home/home.module';
+import { AnimeModule } from './components/anime/anime.module';
+import { HeroesModule } from './components/heroes/heroes.module';
+import { QuotesModule } from './components/quotes/quotes.module';
 
 @NgModule({
   declarations: [AppComponent, DeleteDialogComponent, HeaderComponent],
