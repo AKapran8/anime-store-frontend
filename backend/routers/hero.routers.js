2de02d426a7563eefa4cae3749badd0c99a41152
Andrii Kapran (Work Acc) <a.kapran@do-it.co>
2023-04-30 20:17:30 +0200

    transported image creator to another file

--- backend/routers/hero.routers.js
+++ backend/routers/hero.routers.js
@@ -1,38 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
 
 const heroController = require("./../controllers/heroes.controller");
 
 const checkAuth = require("./../middleware/check-auth");
-
-const MIME_TYPE_HELPER = {
-  "image/png": "png",
-  "image/jpeg": "jpeg",
-  "image/jpg": "jpg",
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_HELPER[file.mimetype];
-    cb(!isValid ? new Error("Invalid type") : null, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const imgName = file.originalname.replace(/\s/g, "").toLowerCase();
-    const ext = MIME_TYPE_HELPER[file.mimetype];
-    const name = `${imgName}.${ext}`;
-    cb(null, name);
-  },
-});
+const checkImage = require("./../middleware/check-image");
 
 router.get("", checkAuth, heroController.getHeroes);
 router.get("/names", checkAuth, heroController.getHeroNames);
-router.post(
-  "",
-  checkAuth,
-  multer({ storage }).single("image"),
-  heroController.addNewHero
-);
+router.post("", checkAuth, checkImage, heroController.addNewHero);
 router.put("/:id", checkAuth, heroController.editHero);
 router.delete("/:id", checkAuth, heroController.deleteHero);
 
