37d5c3a4fa96b3124c3548886182326f62937be7
Andrii Kapran (Work Acc) <a.kapran@do-it.co>
2023-05-01 08:44:52 +0200

    image creator to another file

--- src/app/components/quotes/service/quotes.service.ts
+++ src/app/components/quotes/service/quotes.service.ts
@@ -2,29 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { IAddEditQuote, IAddEditQuoteRespone, IGetQuotesResponse } from '../quote.model';
+import {
+  IAddEditQuote,
+  IAddEditQuoteRespone,
+  IGetQuotesResponse,
+} from '../quote.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class QuotesService {
+  private _url: string = `${environment.apiUrl}/quotes`;
+
   constructor(private _http: HttpClient) {}
 
   public getQuotes(): Observable<IGetQuotesResponse> {
-    return this._http.get<IGetQuotesResponse>(
-      'http://localhost:3000/api/quotes'
-    );
+    return this._http.get<IGetQuotesResponse>(this._url);
   }
 
   public addQuote(body: IAddEditQuote): Observable<IAddEditQuoteRespone> {
-    return this._http.post<IAddEditQuoteRespone>(
-      'http://localhost:3000/api/quotes',
-      body
-    );
+    return this._http.post<IAddEditQuoteRespone>(this._url, body);
   }
 
   public deleteQuote(id: string): Observable<{ message: string }> {
-    return this._http.delete<{ message: string }>(
-      `http://localhost:3000/api/quotes/${id}`
-    );
+    return this._http.delete<{ message: string }>(`${this._url}/${id}`);
   }
 
   public editQuote(
@@ -32,7 +32,7 @@ export class QuotesService {
     id: string
   ): Observable<IAddEditQuoteRespone> {
     return this._http.put<IAddEditQuoteRespone>(
-      `http://localhost:3000/api/quotes/${id}`,
+      `${this._url}/${id}`,
       requestBody
     );
   }
